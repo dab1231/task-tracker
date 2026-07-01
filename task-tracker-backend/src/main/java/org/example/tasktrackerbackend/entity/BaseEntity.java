67da9561a6150ae564ad0e401292154ec4594329
dab1213 <popaorlov@gmail.com>
2026-07-01 12:31:02 +0400

    fix(sql): correct formatting in users table creation script and update changelog

--- task-tracker-backend/src/main/java/org/example/tasktrackerbackend/entity/BaseEntity.java
+++ task-tracker-backend/src/main/java/org/example/tasktrackerbackend/entity/BaseEntity.java
@@ -2,7 +2,7 @@ package org.example.tasktrackerbackend.entity;
 
 import java.io.Serializable;
 
-public interface BaseEntity<T extends Serializable>  {
+public interface BaseEntity<T extends Serializable> {
 
     T getId();
 }
