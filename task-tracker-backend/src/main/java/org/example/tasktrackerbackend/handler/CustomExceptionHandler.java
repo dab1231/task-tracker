305208acc5b88bf15552ac9d54d8e0f16b5ce5a0
dab1213 <popaorlov@gmail.com>
2026-07-02 21:22:18 +0400

    feat(exception): enhance global exception handling with additional error responses

--- task-tracker-backend/src/main/java/org/example/tasktrackerbackend/handler/CustomExceptionHandler.java
+++ task-tracker-backend/src/main/java/org/example/tasktrackerbackend/handler/CustomExceptionHandler.java
@@ -7,19 +7,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<ErrorResponse> handleUserAlreadyExistsException(Exception ex) {
+    public ResponseEntity<ErrorResponse> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
         var message = ex.getMessage();
         log.warn(message);
 
-        return new ResponseEntity<>(new ErrorResponse(message), HttpStatus.CONFLICT);
+        return new ResponseEntity<>(
+                new ErrorResponse(message),
+                HttpStatus.CONFLICT
+        );
     }
 
     @ExceptionHandler({UsernameNotFoundException.class, BadCredentialsException.class})
@@ -30,6 +37,28 @@ public class CustomExceptionHandler {
         return new ResponseEntity<>(
                 new ErrorResponse("Email or password is not correct"),
                 HttpStatus.UNAUTHORIZED
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+
+        var fieldErrors = ex.getBindingResult().getFieldErrors();
+        var message = fieldErrors.stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining(" "));
+        return new ResponseEntity<>(new ErrorResponse(message), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleUnexpectedException(Exception ex) {
+
+        var message = ex.getMessage();
+        log.warn(message);
+
+        return new ResponseEntity<>(
+                new ErrorResponse(message),
+                HttpStatus.INTERNAL_SERVER_ERROR
         );
     }
 }
