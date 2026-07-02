d531ea943b20d503fff61a738dff8fa39d37c3ac
dab1213 <popaorlov@gmail.com>
2026-07-02 21:08:39 +0400

    feat(validation): add validation annotations to UserRequest and update API methods

--- task-tracker-backend/src/main/java/org/example/tasktrackerbackend/controller/api/UserApi.java
+++ task-tracker-backend/src/main/java/org/example/tasktrackerbackend/controller/api/UserApi.java
@@ -4,6 +4,7 @@ import org.example.tasktrackerbackend.dto.request.UserRequest;
 import org.example.tasktrackerbackend.dto.response.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public interface UserApi {
 
     @PostMapping
-    ResponseEntity<Void> registerUser(@RequestBody UserRequest userRequest);
+    ResponseEntity<Void> registerUser(@Validated @RequestBody UserRequest userRequest);
 
     @GetMapping
     ResponseEntity<UserResponse> getCurrentUser(@AuthenticationPrincipal String email);
