8d0ca9e0d0cb389c5699bbffbdcd38a31bde2b32
dab1213 <popaorlov@gmail.com>
2026-07-09 17:01:46 +0400

    feat(security): implement InternalFilter for API key validation

---
+++ task-tracker-backend/src/main/java/org/example/tasktrackerbackend/security/InternalFilter.java
@@ -0,0 +1,39 @@
+package org.example.tasktrackerbackend.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+@Component
+public class InternalFilter extends OncePerRequestFilter {
+
+    @Value("${internal.api.key}")
+    private String internalKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var keyHeader = request.getHeader("X-Internal-Api-Key");
+        if (Objects.equals(keyHeader, internalKey)) {
+
+            var authorityList = AuthorityUtils.createAuthorityList("INTERNAL_SERVICE");
+            var authenticationToken = new UsernamePasswordAuthenticationToken("internal-service", "internal-service", authorityList);
+            var emptyContext = SecurityContextHolder.createEmptyContext();
+
+            emptyContext.setAuthentication(authenticationToken);
+            SecurityContextHolder.setContext(emptyContext);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
