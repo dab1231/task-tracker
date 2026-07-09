573deee668384834675d5f7e03385bc049cdc5c4
dab1213 <popaorlov@gmail.com>
2026-07-09 17:07:37 +0400

    feat(security): enhance InternalFilter and update security configuration

--- task-tracker-backend/src/main/java/org/example/tasktrackerbackend/security/InternalFilter.java
+++ task-tracker-backend/src/main/java/org/example/tasktrackerbackend/security/InternalFilter.java
@@ -4,16 +4,15 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Objects;
 
 @Component
@@ -23,7 +22,7 @@ public class InternalFilter extends OncePerRequestFilter {
     private String internalKey;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         var keyHeader = request.getHeader("X-Internal-Api-Key");
         if (Objects.equals(keyHeader, internalKey)) {
 
